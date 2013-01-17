0788749b8f8748f2a9200b9610df6c8d7aabbb81
adlenafane <adlen.afane@gmail.com>
2013-01-17 20:41:50 +0100

    Added the missing arguments

--- master.py
+++ master.py
@@ -1,5 +1,5 @@
 from mpi4py import MPI
 
-def comportementMaitre():
+def comportementMaitre(comm):
 	print "Hello! I'm rank %d from %d running in total..." % (comm.rank, comm.size)
 	return
--- slave.py
+++ slave.py
@@ -1,5 +1,5 @@
 from mpi4py import MPI
 
-def comportementEsclave():
+def comportementEsclave(comm):
 	print "Hello! I'm rank %d from %d running in total..." % (comm.rank, comm.size)
 	return

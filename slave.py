dcf11b19605c8711b37d571d00a59f2603714045
adlenafane <adlen.afane@gmail.com>
2013-01-19 17:25:31 +0100

    Comments on the next things to be done

--- slave.py
+++ slave.py
@@ -34,3 +34,35 @@ def comportementEsclave(comm):
     
     print "Termine"
     comm.send(resultat,dest=0,tag=4)
+
+"""
+Modification à implémenter quand on comprendra mieux le fonctionnement des messages, sinon les esclaves ne pourront que faire une tâche et/ou ne jamais s'arrêter
+def comportementEsclave(comm):
+    print "Hello! I'm rank %d from %d running in total..." % (comm.rank, comm.size)
+    problemeFini = False
+    while(problemeFini != True):
+        data=comm.recv(source=0, tag=MPI.ANY_TAG)
+        status = MPI.Request()
+        if status.Get_tag()==1:
+            print "Received " + str(data)
+            resultat=[]
+            for probleme in data:
+                valeursVariables,resultatPreTraitement=preTraitementSat(probleme)
+                if resultatPreTraitement==True:
+                    #On envoie un message de type tag 2 au maître pour lui indiquer que le pb SAT a ete resolu
+                    #On envoie au maitre la valeur des variables resolvant le probleme SAT
+                    comm.send(valeursVariables,dest=0,tag=2)
+                    problemeFini = True
+
+                elif resultatPreTraitement==False:
+                    #On envoie un message de type tag 3 au maître pour lui indiquer que le pb ne peut pas etre resolu
+                    comm.send(dest=0,tag=3)
+                else:
+                    resultat.append(genererSousSat(valeursVariables,resultatPreTraitement)[0])
+                    resultat.append(genererSousSat(valeursVariables,resultatPreTraitement)[1])
+            
+            print "Termine"
+            comm.send(resultat,dest=0,tag=4)
+        if status.Get_tag()==2:
+            problemeFini = True
+"""

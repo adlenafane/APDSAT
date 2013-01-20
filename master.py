d62b13eb178872e82392228a93f2355ad05e5d0d
adlenafane <adlen.afane@gmail.com>
2013-01-20 18:12:54 +0100

    Master and slave can now communicate

--- master.py
+++ master.py
@@ -4,6 +4,7 @@ from utility import loadCnfFile, calculVariablesPourBranching, calculClassementL
 import Queue
 from math import log
 import sys
+from array import array
 
 #Example assez complet en C++ sur un master qui distribue des jobs à ses esclaves: http://www.lam-mpi.org/tutorials/one-step/ezstart.php
 
@@ -57,35 +58,52 @@ def comportementMaitre(comm, filename):
 		print message
 		fileDesPb.put(message)
 		"""
-
-		
+		"""
+		buf = array("c", '\0')*256
+		request_instance = comm.Irecv(buf, source = MPI.ANY_SOURCE)
+		status = MPI.Status()
+		request_instance.Test(status)
+		n = status.Get_count(MPI.CHAR)
+		s = buf[:n].tostring()
+		print s
+		print request_instance
+		print status
+		print status.Get_tag()
+		"""
 		# Autre solution: faire ca sequentiellement. Mais du coup il faudrait aussi faire l'envoi de maniere sequentiel (ca n'optimise pas l'utilisation des processeurs, mais bon, l'avantage c'est que ca simplifie pas mal: plus besoin de la variable esclaveDisponible par exemple, et puis comme les taches sont de tailles similaire, et qu'on va les faire tourner sur le meme processeur, les temps de traitement seront très très très proches, donc a mon avis on n'y perd pas bcp...).
 		#pour le Get_tag, j'ai pas pu tester, mais ca doit etre quelque chose commce ca, cf https://groups.google.com/forum/?fromgroups=#!topic/mpi4py/fHzY1gAEYpM
-		for indexEsclave in range(1,size): 
-			message = []
-			request_instance = comm.irecv()
-			request_instance.Wait()
-			#Tag 2 pour un message de l'esclave vers le maitre indiquant que le pbSAT a ete resolu
-			if status.Get_tag()==2:
-				print "Une solution a ete trouvee, il s'agit de:"
-				print str(message)
-				pbNonFini = False
+		for indexEsclave in range(1,size):
+			if listeEsclave[indexEsclave] == 1:
+				#status = MPI.Status()
+				status = MPI.Status()
+				message = comm.recv(source=indexEsclave, tag = MPI.ANY_TAG, status= status)
+				print status
+				print status.Get_source()
+				print status.Get_tag()
+				#Tag 2 pour un message de l'esclave vers le maitre indiquant que le pbSAT a ete resolu
+				if status.Get_tag()==2:
+					print "Une solution a ete trouvee, il s'agit de:"
+					print str(request_instance)
+					pbNonFini = False
 
-			#Tag 3 pour un message de l'esclave vers le maitre indiquant que le pbSAT ne peut pas etre resolu (une clause est fausse)
-			elif status.get_tag()==3:
-				print "Cette branche n'a pas de solution"
-				# On ne peut pas arrêter la résolution du pb, celui peut avoir une solution sur une autre branche, c'est juste la branche qu'on kill
-				#pbNonFini = False
+				#Tag 3 pour un message de l'esclave vers le maitre indiquant que le pbSAT ne peut pas etre resolu (une clause est fausse)
+				elif status.Get_tag()==3:
+					print "Cette branche n'a pas de solution"
+					# On ne peut pas arrêter la résolution du pb, celui peut avoir une solution sur une autre branche, c'est juste la branche qu'on kill
+					#pbNonFini = False
 
-			#Tag 4 signifie l'esclave envoie un pb au maitre
-			elif status.get_tag()==4:
-				print "la resolution continue avec " + str(message)
-				esclaveDisponible+=1
-				for pb in message:
-					fileDesPb.put(pb)
-		
+				#Tag 4 signifie l'esclave envoie un pb au maitre
+				elif status.Get_tag()==4:
+					print "la resolution continue avec " + str(message)
+					esclaveDisponible+=1
+					for pb in message:
+						fileDesPb.put(pb)
+						print pb
+					pbNonFini = True
+		# Testing purpose
 		pbNonFini = False
-	message = ""
+	message = "message de fin"
+	print message
 	for indexEsclave in range(1, size):
-		comm.send(message, dest=indexEsclave, tag=2)
+		comm.send(message, dest=indexEsclave, tag=5)
 	return
--- slave.py
+++ slave.py
@@ -8,61 +8,34 @@ from retireSingleton import *
 #Tag 2 pour un message de l'esclave vers le maitre indiquant que le pbSAT a ete resolu
 #Tag 3 pour un message de l'esclave vers le maitre indiquant que le pbSAT ne peut pas etre resolu (une clause est fausse)
 #Tag 4 signifie l'esclave envoie un pb au maitre
-
+#Tag 5 signifie que le problème est terminé
 
 def comportementEsclave(comm):
-    print "Hello! I'm rank %d from %d running in total..." % (comm.rank, comm.size)
-        
-        
-    data=comm.recv(source=0,tag=1)
+    print "Hello! I'm rank %d from %d running in total..." % (comm.rank, comm.size)    
+    status = MPI.Status()
+    data=comm.recv(source=0,tag = MPI.ANY_TAG, status= status)
     print "Received " + str(data)
-    resultat=[]
-    for probleme in data:
-        valeursVariables,resultatPreTraitement=preTraitementSat(probleme)
-        
-        if resultatPreTraitement==True:
-            #On envoie un message de type tag 2 au maître pour lui indiquer que le pb SAT a ete resolu
-            #On envoie au maitre la valeur des variables resolvant le probleme SAT
-            comm.send(valeursVariables,dest=0,tag=2)
-        
-        elif resultatPreTraitement==False:
-            #On envoie un message de type tag 3 au maître pour lui indiquer que le pb ne peut pas etre resolu
-            comm.send(dest=0,tag=3)
-        else:
-            resultat.append(genererSousSat(valeursVariables,resultatPreTraitement)[0])
-            resultat.append(genererSousSat(valeursVariables,resultatPreTraitement)[1])
-    
-    print "Termine"
-    comm.send(resultat,dest=0,tag=4)
 
-"""
-Modification à implémenter quand on comprendra mieux le fonctionnement des messages, sinon les esclaves ne pourront que faire une tâche et/ou ne jamais s'arrêter
-def comportementEsclave(comm):
-    print "Hello! I'm rank %d from %d running in total..." % (comm.rank, comm.size)
-    problemeFini = False
-    while(problemeFini != True):
-        data=comm.recv(source=0, tag=MPI.ANY_TAG)
-        status = MPI.Request()
-        if status.Get_tag()==1:
-            print "Received " + str(data)
-            resultat=[]
-            for probleme in data:
-                valeursVariables,resultatPreTraitement=preTraitementSat(probleme)
-                if resultatPreTraitement==True:
-                    #On envoie un message de type tag 2 au maître pour lui indiquer que le pb SAT a ete resolu
-                    #On envoie au maitre la valeur des variables resolvant le probleme SAT
-                    comm.send(valeursVariables,dest=0,tag=2)
-                    problemeFini = True
-
-                elif resultatPreTraitement==False:
-                    #On envoie un message de type tag 3 au maître pour lui indiquer que le pb ne peut pas etre resolu
-                    comm.send(dest=0,tag=3)
-                else:
-                    resultat.append(genererSousSat(valeursVariables,resultatPreTraitement)[0])
-                    resultat.append(genererSousSat(valeursVariables,resultatPreTraitement)[1])
+    while status.Get_tag() != 5:
+        resultat=[]
+        for probleme in data:
+            valeursVariables,resultatPreTraitement=preTraitementSat(probleme)
             
-            print "Termine"
-            comm.send(resultat,dest=0,tag=4)
-        if status.Get_tag()==2:
-            problemeFini = True
-"""
+            if resultatPreTraitement==True:
+                #On envoie un message de type tag 2 au maître pour lui indiquer que le pb SAT a ete resolu
+                #On envoie au maitre la valeur des variables resolvant le probleme SAT
+                comm.send(valeursVariables,dest=0,tag=2)
+            
+            elif resultatPreTraitement==False:
+                #On envoie un message de type tag 3 au maître pour lui indiquer que le pb ne peut pas etre resolu
+                comm.send(dest=0,tag=3)
+            else:
+                resultat.append(genererSousSat(valeursVariables,resultatPreTraitement)[0])
+                resultat.append(genererSousSat(valeursVariables,resultatPreTraitement)[1])
+        
+        print "Termine"
+        comm.send(resultat,dest=0,tag=4)
+        print "Sent"
+        data=comm.recv(source=0,tag = MPI.ANY_TAG, status= status)
+        print "New message Received"
+    return

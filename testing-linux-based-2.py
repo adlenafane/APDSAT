1b59c908b42aed4b78b41c53804375d0f1566ee0
edelans <edelans@gmail.com>
2013-01-21 04:00:09 +0100

    new data

--- testing-linux-based-2.py
+++ testing-linux-based-2.py
@@ -9,3 +9,9 @@ for tailleDeBatch in [2**x for x in range(2,10)]:
 			os.system("mpiexec -n 4 python main.py" + " --file "+ "data/" + str(filename) +" --batch " + str(tailleDeBatch))
 os.system("python sequentiel.py" + " --file "+ "data/" +  str(filename))
 
+for tailleDeBatch in [2**x for x in range(2,10)]:
+	for filename in listing:
+		for i in range(1, 2):
+			os.system("mpiexec -n 4 python main_random.py" + " --file "+ "data/" + str(filename) +" --batch " + str(tailleDeBatch))
+os.system("python sequentiel.py" + " --file "+ "data/" +  str(filename))
+
